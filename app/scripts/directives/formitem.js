d01be606816596fd2a42008a6d2d217b632ec56b
marlonmleite <marlonmleite@gmail.com>
2014-10-01 13:15:39 -0300

    Renderizando item a partir de informacoes informadas ao form item

--- app/scripts/directives/formitem.js
+++ app/scripts/directives/formitem.js
@@ -8,6 +8,8 @@
  */
 angular.module('entityFormApp')
   .directive('formItem', function () {
+  	var defaultDateFormat = "dd/MM/yyyy";
+  	
     var controllerFormItem = function($scope) {
 		$scope.open = function(event) {
 		    event.preventDefault();
@@ -16,12 +18,39 @@ angular.module('entityFormApp')
 		    $scope.opened = true;
 		};
 
+		$scope.buildField = function(){
+			$scope.field = {};
+			$scope.field.fieldId = $scope.fieldId;
+			$scope.field.fieldName = $scope.fieldName;
+			$scope.field.fieldType = $scope.fieldType;
+			$scope.field.fieldRequired = $scope.fieldRequired;
+			$scope.field.fieldDisabled = $scope.fieldDisabled;
+			$scope.field.fieldDescription = $scope.fieldDescription;
+
+			if ($scope.dateFormat === undefined || $scope.dateFormat == "" || $scope.dateFormat == null) {
+				$scope.dateFormat = defaultDateFormat;
+			}
+		};
+
 	};
 
 	return {
 		controller: controllerFormItem,
 		templateUrl: 'views/directives/formitem.html',
 		restrict: 'E',
-		scope: { field: '=' }
+		scope: { 
+			field: '=field',
+			fieldId: '@fieldId',
+			fieldName: '@fieldName',
+			fieldType: '@fieldType',
+			fieldRequired: '@fieldRequired',
+			fieldDisabled: '@fieldDisabled',
+			fieldDescription: '@fieldDescription'
+		},
+		link: function(scope, element, attrs) {
+			if (scope.field === undefined || scope.field == "" || scope.field == null) {
+				scope.buildField();
+			}
+  		}
 	};
   });
