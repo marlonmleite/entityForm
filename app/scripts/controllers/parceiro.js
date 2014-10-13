1b590449c00671c1b77cf7813025be7e72efea75
marlonmleite <marlonmleite@gmail.com>
2014-10-13 09:28:14 -0300

    Implementando metadados da entidade Parceiro

--- app/scripts/controllers/parceiro.js
+++ app/scripts/controllers/parceiro.js
@@ -8,10 +8,26 @@
  * Controller of the entityFormApp
  */
 angular.module('entityFormApp')
-  .controller('ParceiroCtrl', function ($scope) {
+  .controller('ParceiroCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('json/parceiro/getMetadadosResponse.json').then(function(response){
+    	var entitys = response.data.responseBody.metadata.entity;
+
+    	angular.forEach(entitys, function(value){
+    		var sanUtils = new SanUtils();
+
+    		if (value.name == "Parceiro") {
+    			var metadadosParceiro = value;
+
+    			sanUtils.orderMetadados(metadadosParceiro);
+
+    			$scope.metadados = metadadosParceiro;
+    		}
+    	});
+    });
   });
